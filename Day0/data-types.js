1bf7fb151ab6c2f1619802173a96c6366407c956
Carlos-SSH <carlos.anezeh@gmail.com>
2020-10-27 07:39:12 -0600

    Data Types Solution Created.

---
+++ Day0/data-types.js
@@ -0,0 +1,10 @@
+   
+// Write code that uses console.log to print the sum of the 'firstInteger' and 'secondInteger' (converted to a Number        type) on a new line.
+    console.log(parseInt(firstInteger) + parseInt(secondInteger));
+    
+// Write code that uses console.log to print the sum of 'firstDecimal' and 'secondDecimal' (converted to a Number            type) on a new line.
+    console.log(parseFloat(firstDecimal) + parseFloat(secondDecimal));
+    
+// Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
+    console.log(firstString + secondString);
+
